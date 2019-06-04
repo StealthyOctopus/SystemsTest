92320b0e327f9cb9cbf8f4be669e63f09cc38bf9
StealthyOcto <barneyrandall@outlook.com>
2019-06-04 21:16:13 +0100

    Added function defs for adding power management systems to power manager

--- src/components/PowerManager.java
+++ src/components/PowerManager.java
@@ -1,14 +1,28 @@
 package components;
 
+import java.util.*;
+
 import core.Tickable;
+import components.powered.*;
 
 public class PowerManager implements Tickable {
 	
 	private Reactor reactor;
+	
+	//map our powered systems by priority, with the array list allowing us to have several systems at the same priority
+	private Map<Integer, ArrayList<PoweredSystemBase>> poweredSystems;
 		
 	public PowerManager(Reactor reactor)
 	{
 		this.reactor = reactor;
+	}
+	
+	public void AddPoweredSystem(PoweredSystemBase system) {
+		this.AddPoweredSystem(system, 0);
+	}
+	
+	public void AddPoweredSystem(PoweredSystemBase system, Integer priority) {
+		
 	}
 
 	@Override
